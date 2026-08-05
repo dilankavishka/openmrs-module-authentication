1b9c3494d426703736199c8578a4eb998b3ec35a
dilankavishka <dkgamage01@gmail.com>
2026-08-05 22:10:04 +0530

    Minor change

--- omod/src/main/java/org/openmrs/module/authentication/web/TwoFactorAuthenticationScheme.java
+++ omod/src/main/java/org/openmrs/module/authentication/web/TwoFactorAuthenticationScheme.java
@@ -330,7 +330,7 @@ public class TwoFactorAuthenticationScheme extends WebAuthenticationScheme {
 		List<String> schemeIds = getSecondaryAuthenticationSchemeIdsForUser(user);
 		if (!schemeIds.contains(schemeId)) {
 			schemeIds.add(schemeId);
-			Context.getUserService().setUserProperty(user, USER_PROPERTY_SECONDARY_TYPE, String.join(",", schemeId));
+			Context.getUserService().setUserProperty(user, USER_PROPERTY_SECONDARY_TYPE, String.join(",", schemeIds));
 		}
 	}
 
